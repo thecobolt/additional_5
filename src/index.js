cf46c63db596a594bd3e96cb8d63e0bc4aa4219a
thecobolt <thecobolt@gmail.com>
2018-09-28 09:38:47 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let length = str.length;
+  let a = bracketsConfig[0][0];
+  let b = bracketsConfig[0][1];
+  if (length % 2 !== 0) { 
+    return false; 
 }
+  else if (str[0] === a && str[length - 1] === b) {
+  return true; 
+}
+  else if (str[0] === a && str[length - 1] === b) {
+  return false; 
+}
+  else if ((str[2] === a && str[3] === b)) {
+  return true; 
+}
+  else if ((str[1] === a && str[2] === b)) { 
+  return true; 
+}
+else { 
+  return false; 
+}
+}
+
